f27f4a8df2e35b9a2944ae6d645e11deccb8bb25
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2024-04-14 15:12:25 +0300

    Add basic running program

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.9f, 0.6f, 0.9f, 1.0f);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
